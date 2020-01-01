af628200a326b5204b541214e9d512c16bbf1b42
aditya nehra <asniii.coder@gmail.com>
2020-01-01 23:01:28 +0530

    tic tac toe - making interactive components

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ import * as serviceWorker from './serviceWorker';
 
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
+
     render() {
+
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value:'X'})}>
+                {this.state.value}
             </button>
         );
     }
